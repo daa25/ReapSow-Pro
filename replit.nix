04ba09cf78dd78c9a1e566b0114f7d68118b8cf2
VibeFlex Sports <153103162+daa25@users.noreply.github.com>
2025-07-12 02:04:15 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.firebase-admin
-    pkgs.python311Packages.requests
-    pkgs.python311Packages.python-dotenv
-  ];
-}
